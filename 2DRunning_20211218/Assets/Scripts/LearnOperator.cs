befe6d5df00abb750c4a443ea4d7f339e31e429e
KID <50666207+KID421@users.noreply.github.com>
2022-02-12 16:58:49 +0800

    Update 運算子

--- 2DRunning_20211218/Assets/Scripts/LearnOperator.cs
+++ 2DRunning_20211218/Assets/Scripts/LearnOperator.cs
@@ -2,22 +2,61 @@ using UnityEngine;
 
 public class LearnOperator : MonoBehaviour
 {
+    public float a = 10;
+    public float b = 3;
+    public int c = 100;
+    public int d = 9;
+
     private void Start()
     {
         #region 運算子：數學
         // 加 +
+        print("加法：" + (a + b));     // 13
         // 減 -
+        print("減法：" + (a - b));     // 7
         // 乘 *
+        print("乘法：" + (a * b));     // 30
         // 除 /
+        print("除法：" + (a / b));     // 3.333
         // 餘 %
+        print("餘法：" + (a % b));     // 1
         #endregion
 
         #region 運算子：比較
-
+        // 比較後的結果為布林值：true、false
+        // 大於 >
+        print("大於：" + (c > d));         // true
+        // 小於 <
+        print("小於：" + (c < d));         // false
+        // 大於等於 >=
+        print("大於等於：" + (c >= d));     // true
+        // 小於等於 <=
+        print("小於等於：" + (c <= d));     // false
+        // 等於 ==
+        print("等於：" + (c == d));         // false
+        // 不等於 !=
+        print("不等於：" + (c != d));       // true
         #endregion
 
         #region 運算子：邏輯
-
+        // 比較後的結果為布林值：true、false
+        // 邏輯運算子是針對布林值的運算
+        // 並且 && (shift + 7)
+        // 只要其中一個布林值為 false 結果就是 false
+        print(true && true);        // true
+        print(true && false);       // false
+        print(false && true);       // false
+        print(false && false);      // false
+        // 或者 || (shift + 鎮)
+        // 只要其中一個布林值為 true 結果就是 true
+        print(true || true);        // true
+        print(true || false);       // true
+        print(false || true);       // true
+        print(false || false);      // false
+        // 顛倒 !
+        // 將布林值變相反
+        print(!true);               // false
+        print(!false);              // true
         #endregion
     }
 }
