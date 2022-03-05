15af25b110179da5d7466aae4ead1331fbe1e92e
KID <50666207+KID421@users.noreply.github.com>
2022-03-05 17:00:13 +0800

    Update 玩家跳躍功能

--- 2DRunning_20211218/Assets/Scripts/Player.cs
+++ 2DRunning_20211218/Assets/Scripts/Player.cs
@@ -24,15 +24,35 @@ public class Player : MonoBehaviour
     public string parameterJump = "觸發跳躍";
     public string parameterSlide = "開關滑行";
     public string parameterDead = "觸發死亡";
-    #endregion
+
+    [Header("跳躍按鍵")]
+    public KeyCode keyJump = KeyCode.Space;
 
     // 存取 Transform 第一種方式
     // public Transform traPlayer;
+    #endregion
+
+    // 屬性面板 ... > Debug 模式可以看到私人資料
+    private Rigidbody2D rig;
+    [Header("跳躍段數最大值"), Range(0, 5)]
+    public int countJumpMax = 2;
+
+    public int countJump;
 
     #region 事件
+    private void Start()
+    {
+        // GetComponent<元件類型>() - <> 泛型，所有類型
+        // 取得指定元件
+        rig = GetComponent<Rigidbody2D>();
+        // 跳躍段數 指定為 最大值
+        countJump = countJumpMax;
+    }
+
     private void Update()
     {
         Run();
+        Jump();
     }
     #endregion
 
@@ -52,6 +72,26 @@ public class Player : MonoBehaviour
         // 語法：
         // transform.成員名稱
         transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        // 是否按下跳躍 = 輸入.取得按鍵按下(指定按鍵)
+        bool inputJump = Input.GetKeyDown(keyJump);
+        // print("是否按下跳躍：" + inputJump);
+
+        // 如果 按下跳躍 並且 跳躍段數 大於 零 就 往上跳
+        if (inputJump && countJump > 0)
+        {
+            // print("跳躍");
+            // 剛體.添加推力(二維向量)
+            rig.AddForce(new Vector2(0, jump));
+            // 跳躍之後，跳躍段數減一
+            countJump--;
+        }
     }
     #endregion
 }
