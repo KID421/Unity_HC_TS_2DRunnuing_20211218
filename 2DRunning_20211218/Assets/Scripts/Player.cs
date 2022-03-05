27e623d13bd90f232fb27a01bc42dc05860c2cbc
KID <50666207+KID421@users.noreply.github.com>
2022-03-05 20:55:35 +0800

    Update 角色滑行動畫與碰撞器更新

--- 2DRunning_20211218/Assets/Scripts/Player.cs
+++ 2DRunning_20211218/Assets/Scripts/Player.cs
@@ -37,7 +37,6 @@ public class Player : MonoBehaviour
     public int countJumpMax = 2;
 
     public int countJump;
-    #endregion
 
     [Header("檢查地板位移")]
     public Vector3 v3GroudOffset;
@@ -45,6 +44,17 @@ public class Player : MonoBehaviour
     public Vector3 v3GroundSize = Vector3.one;
     [Header("地板的圖層")]
     public LayerMask layerGround;
+
+    // private Animation aniOld;        // 舊版 動畫系統
+    // private Animator aniNew;         // 新版 動畫系統
+
+    private Animator ani;
+    #endregion
+
+    [Header("滑行按鍵")]
+    public KeyCode keySlide = KeyCode.DownArrow;
+
+    private CapsuleCollider2D cc2d;
 
     #region 事件
     // 繪製圖示事件：在 Unity 內繪製輔助用的圖示，包含：線、方形、圓形等幾何圖形 (執行檔不會顯示)
@@ -62,6 +72,9 @@ public class Player : MonoBehaviour
         // GetComponent<元件類型>() - <> 泛型，所有類型
         // 取得指定元件
         rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        cc2d = GetComponent<CapsuleCollider2D>();
+
         // 跳躍段數 指定為 最大值
         countJump = countJumpMax;
     }
@@ -70,6 +83,7 @@ public class Player : MonoBehaviour
     {
         Run();
         Jump();
+        Slide();
     }
     #endregion
 
@@ -108,18 +122,48 @@ public class Player : MonoBehaviour
             rig.AddForce(new Vector2(0, jump));
             // 跳躍之後，跳躍段數減一
             countJump--;
+            // 動畫.設定觸發(動畫參數名稱)
+            ani.SetTrigger(parameterJump);
         }
 
         // 2D 碰撞 = 2D 物理.方形覆蓋(中心點，尺寸，角度，圖層)
         Collider2D hit = Physics2D.OverlapBox(transform.position + v3GroudOffset, v3GroundSize, 0, layerGround);
 
-        print("玩家的重力加速度：" + rig.velocity);
+        // print("玩家的重力加速度：" + rig.velocity);
 
         // 如果 2D 碰撞物件存在 並且 剛體的加速度 Y < 0 (往下掉落)
         if (hit && rig.velocity.y < 0)
         {
             // 跳躍次數 指定 最大跳躍次數
             countJump = countJumpMax;
+        }
+    }
+
+    /// <summary>
+    /// 滑行
+    /// </summary>
+    private void Slide()
+    {
+        // 判斷是否按下滑行
+        // 更新滑行動畫
+        // 更新碰撞器
+        if (Input.GetKey(keySlide))
+        {
+            ani.SetBool(parameterSlide, true);
+
+            // 滑行：0.2, -0.85 | 2, 1.2 h
+            cc2d.offset = new Vector2(0.2f, -0.85f);
+            cc2d.size = new Vector2(2, 1.2f);
+            cc2d.direction = CapsuleDirection2D.Horizontal;
+        }
+        else
+        {
+            ani.SetBool(parameterSlide, false);
+
+            // 站立：0.2, -0.2  | 1, 2.5 v
+            cc2d.offset = new Vector2(0.2f, -0.2f);
+            cc2d.size = new Vector2(1, 2.5f);
+            cc2d.direction = CapsuleDirection2D.Vertical;
         }
     }
     #endregion
